0486ad9c157e01a0b165df5d796933258f1e2749
JaeAnHan <jaean1999@naver.com>
2024-04-30 14:02:33 +0900

    feat: 사용자 입력 받기
    유효하지 않을 경우 alert후 입력창 초기화

--- src/main.js
+++ src/main.js
@@ -15,6 +15,17 @@ const RESET_BUTTON_CONTENT = "게임 재시작";
 const ALERT_DIGITS_NOT_LONG_ENOUGH = "최소 3자리의 숫자를 입력해 주세요";
 const ALERT_DIGITS_NOT_DISTINCT = "중복되는 숫자 없이 주세요";
 
+const selectElements = () => {
+    const userInput = document.querySelector(USER_INPUT);
+    const submitButton = document.querySelector(SUBMIT_BUTTON);
+    const resultBoard = document.querySelector(RESULT_BOARD);
+
+    return new Map()
+        .set(USER_INPUT, userInput)
+        .set(SUBMIT_BUTTON, submitButton)
+        .set(RESULT_BOARD, resultBoard);
+}
+
 const randomDigitString = (min, max) => ("" + Math.floor(Math.random() * (max - min) + min));
 
 const generateNewDistinct3Digits = () => {
@@ -27,4 +38,37 @@ const generateNewDistinct3Digits = () => {
     console.log('정답', digitSet);
 
     return [...digitSet];
+}
+
+const declineNonNumericInput = (e) => {
+    if ((e.keyCode >= 48 && e.keyCode <= 57)
+        || e.keyCode === BACKSPACE
+        || e.keyCode === ENTER) {
+        return;
+    }
+    e.preventDefault();
+}
+
+const isInputValue3Length = (inputValue) => {
+    return inputValue.length === 3;
+}
+
+const isInputValueAllDistinct = (inputValue) => {
+    return new Set(inputValue.split('')).size === 3;
+}
+
+const isInputInvalid = (inputValue, resetUserInput) => {
+    if (!isInputValue3Length(inputValue)) {
+        alert(ALERT_DIGITS_NOT_LONG_ENOUGH);
+        resetUserInput();
+        return true;
+    }
+
+    if (!isInputValueAllDistinct(inputValue)) {
+        alert(ALERT_DIGITS_NOT_DISTINCT);
+        resetUserInput();
+        return true;
+    }
+
+    return false;
 }
