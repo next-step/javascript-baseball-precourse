17deaab2573b7f30cf5914d7e11d22203490b49e
JaeAnHan <jaean1999@naver.com>
2024-04-30 20:38:55 +0900

    feat: 사용자 입력 제출시 애니메이션 추가
    animation-duration 변경가능

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const DIGIT_MAX = 9;
 const USER_INPUT = "#user-input";
 const SUBMIT_BUTTON = "#submit-button";
 const RESULT_BOARD = "#result-board";
-// const RESULT_HEAD = "#result-head";
+const RESULT_HEAD = "#result-head";
 const BACKSPACE = 8;
 const ENTER = 13;
 const STRIKE_POINT = 10;
@@ -16,18 +16,20 @@ const RESET_BUTTON_CONTENT = "게임 재시작";
 const ALERT_DIGITS_NOT_LONG_ENOUGH = "최소 3자리의 숫자를 입력해 주세요";
 const ALERT_DIGITS_NOT_DISTINCT = "중복되는 숫자 없이 주세요";
 const NOT_FOUND_VALUE = -1
+// 변경 가능
+const ANIMATION_DURATION = 700;
 
 const selectElements = () => {
     const userInput = document.querySelector(USER_INPUT);
     const submitButton = document.querySelector(SUBMIT_BUTTON);
     const resultBoard = document.querySelector(RESULT_BOARD);
-    // const resultHead = document.querySelector(RESULT_HEAD);
+    const resultHead = document.querySelector(RESULT_HEAD);
 
     return new Map()
         .set(USER_INPUT, userInput)
         .set(SUBMIT_BUTTON, submitButton)
-        .set(RESULT_BOARD, resultBoard);
-        // .set(RESULT_HEAD, resultHead);
+        .set(RESULT_BOARD, resultBoard)
+        .set(RESULT_HEAD, resultHead);
 }
 
 const randomDigitString = (min, max) => ("" + Math.floor(Math.random() * (max - min) + min));
@@ -93,7 +95,6 @@ const countStrikeAndBall = (currentValue, targetDigits) => {
         if (targetDigits.indexOf(currentValue[i]) > NOT_FOUND_VALUE) {
             score += BALL_POINT;
         }
-
     }
 
     return score;
@@ -117,7 +118,6 @@ const makeComment = (score) => {
     return comment;
 }
 
-
 const writeOnBoard = (resultBoardRef, headerContent, paragraphContent) => {
     const boardHeader = resultBoardRef.querySelector('h3');
     const boardParagraph = resultBoardRef.querySelector('p');
@@ -128,13 +128,14 @@ const writeOnBoard = (resultBoardRef, headerContent, paragraphContent) => {
 
 const addResetButton = (resultBoardRef, startNewGame) => {
     const resetButton = document.createElement('button');
-    resetButton.innerHTML = RESET_BUTTON_CONTENT;
-
     const removeButton = () => resultBoardRef.removeChild(resetButton);
+
+    resetButton.innerHTML = RESET_BUTTON_CONTENT;
 
     resetButton.addEventListener('click', () => {
         startNewGame();
         removeButton();
+        writeOnBoard(resultBoardRef, BOARD_HEADER_READY, "");
     })
 
     resultBoardRef.appendChild(resetButton);
@@ -159,33 +160,80 @@ const printResultOnBoard = (score, resultBoardRef, startNewGame) => {
     );
 }
 
+const isCharUTF16 = (char) => {
+    return (char.charCodeAt(0) >= 0xD800 && char.charCodeAt(0) <= 0xDBFF)
+        || (char.charCodeAt(0) >= 0xDC00 && char.charCodeAt(0) <= 0xDFFF);
+}
+
+const concatEmoji = (acc, char, i) => {
+    if (isCharUTF16(char)) {
+        acc[0] += char;
+    } else {
+        acc.push(char);
+    }
+    return acc;
+}
+
+const millisToFloat = (delay) => {
+    return "" + (delay / 1000).toFixed(1);
+}
+
+const createSpan = (char, idx, delay) => {
+    const delayFloat = millisToFloat(delay);
+    return `<span style="animation-delay: ${idx * delayFloat}s;animation-duration: ${delayFloat}s">${char}</span>`
+}
+
+const animateResultHeader = (resultHeaderRef, delay) => {
+    const simpleInnerHTML = resultHeaderRef.innerHTML;
+    const tokens = resultHeaderRef.textContent
+        .split('')
+        .reduce(concatEmoji, ['']);
+
+    resultHeaderRef.innerHTML = tokens
+        .map((char, idx) => createSpan(char, idx, delay))
+        .join('');
+
+    setTimeout(() => {
+        resultHeaderRef.innerHTML = simpleInnerHTML;
+    }, delay * tokens.length);
+
+    return tokens.length;
+}
+
 const runGame = () => {
     const elements = selectElements();
     let targetDigits = generateNewDistinct3Digits();
 
     const resetUserInput = () => elements.get(USER_INPUT).value = "";
     const getUserInput = () => elements.get(USER_INPUT).value;
-
     const getResultBoardRef = () => elements.get(RESULT_BOARD);
+    const getResultHeadRef = () => elements.get(RESULT_HEAD);
 
-    const startNewGame = () => {
-        targetDigits = generateNewDistinct3Digits();
-        resetUserInput();
-        writeOnBoard(getResultBoardRef(), BOARD_HEADER_READY, "");
-    };
-
-    elements.get(USER_INPUT).addEventListener('keydown', declineNonNumericInput);
-    elements.get(SUBMIT_BUTTON).addEventListener('click', () => {
-        const userDigits = getUserInput();
-
-        if (isInputInvalid(userDigits, resetUserInput)) return;
-
-        // console.log(elements.get(RESULT_HEAD).innerHTML.split(''));
+    const delayedScoringAndPrinting = (userDigits, delay) => setTimeout(() => {
+        const startNewGame = () => {
+            targetDigits = generateNewDistinct3Digits();
+            resetUserInput();
+        };
 
         const score = countStrikeAndBall(userDigits, targetDigits);
 
         printResultOnBoard(score, getResultBoardRef(), startNewGame);
-    });
+    }, delay);
+
+    const validateAndScoreInput = () => {
+        const userDigits = getUserInput();
+
+        if (isInputInvalid(userDigits, resetUserInput)) {
+            return;
+        }
+
+        const tokenLength = animateResultHeader(getResultHeadRef(), ANIMATION_DURATION);
+
+        delayedScoringAndPrinting(userDigits, tokenLength * ANIMATION_DURATION);
+    };
+
+    elements.get(USER_INPUT).addEventListener('keydown', declineNonNumericInput);
+    elements.get(SUBMIT_BUTTON).addEventListener('click', validateAndScoreInput);
 }
 
 runGame();
