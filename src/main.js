e5083ec4e9b6617586f60e4053e02add684f5ce6
geongyu09 <geongyu09@gmail.com>
2024-05-01 18:46:37 +0900

    feat: 사용자의 input을 받아서 겹치지 않는 3자리 숫자를 받았는지 확인
    사용자의 input이 제출 되는 것을 확인한다.
    input이 제출되면 사용자의 input을 확인한다.
      - 사용자의 input이 3자리인가?
      - 사용자의 input이 숫자인가?
      - 사용자의 input이 중복이 없는가?
    
    사용자의 input이 올바르지 않다면 에러 메시지를 alert로 띄운다.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+const userForm = document.querySelector("#user-form");
+const userFormInput = userForm.querySelector("input");
+
 const ANSWER_MAX_LENGTH = 3;
+
+function checkIsDuplicate(array) {
+  return new Set(array).size !== array.length;
+}
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 10);
@@ -7,15 +14,43 @@ function getRandomNumber() {
 function makeAnswer() {
   const answer = [];
   while (answer.length < ANSWER_MAX_LENGTH) {
-    const number = getRandomNumber();
-    if (!answer.includes(number)) {
-      answer.push(number);
-    }
+    const newNumber = getRandomNumber();
+    const isDuplicate = checkIsDuplicate(answer);
+    if (!isDuplicate) answer.push(newNumber);
   }
   return answer;
 }
 
+function getUserInput() {
+  return userFormInput.value.split("");
+}
+
+function resetUserInput() {
+  userFormInput.value = "";
+}
+
+function checkIsValidateInput(input) {
+  const isValidate =
+    input.length !== ANSWER_MAX_LENGTH &&
+    checkIsDuplicate(input) &&
+    input.some((num) => isNaN(num));
+
+  if (isValidate) return false;
+  return true;
+}
+
 function init() {
   const answer = makeAnswer();
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userInput = getUserInput();
+    const isValidate = checkIsValidateInput(userInput);
+    if (!isValidate) {
+      alert("중복 없이 3자리 숫자를 입력해주세요.");
+      return;
+    }
+    resetUserInput();
+  });
 }
 init();
