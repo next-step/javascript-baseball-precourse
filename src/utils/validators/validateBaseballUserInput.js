509112bbf238b98787366c80579d7baa51939a74
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-01 21:56:07 +0900

    feat: add isInRange exception function

--- src/utils/validators/validateBaseballUserInput.js
+++ src/utils/validators/validateBaseballUserInput.js
@@ -6,7 +6,12 @@ function isNumber(input) {
   return !Number.isNaN(parseInt(input, DECIMAL));
 }
 
+function isInRange(input) {
+  return input.length === NUMBER.LENGTH;
+}
+
 export default function validateBaseballUserInput(input) {
   if (!isNumber(input)) return false;
+  if (!isInRange(input)) return false;
   return true;
 }
