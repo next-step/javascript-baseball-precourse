da952f9f99f1b8c8a24e5037510fae48cb00525e
minji2219 <200146@jnu.ac.kr>
2024-05-01 19:51:32 +0900

    힌트 제공 및 정답 처리
    *정답 입력시 정답 문구 출력
    *낫싱, 스트라이크, 볼 결과 출력

--- src/main.js
+++ src/main.js
@@ -12,14 +12,39 @@ const randomProduce = () => {
   while (firstNum === thirdNum || secondNum === thirdNum) {
     thirdNum = Math.floor(Math.random() * 10);
   }
-  return firstNum * 100 + secondNum * 10 + thirdNum * 1;
+  return [firstNum, secondNum, thirdNum];
 };
 
-console.log(randomProduce());
+const randomNum = randomProduce();
+
+const checkNum = (random, input) => {
+  let strike = 0;
+  let ball = 0;
+  console.log("random", random);
+
+  input.split("").forEach((e, i) => {
+    if (random.indexOf(parseInt(e)) === i) strike++;
+    else if (random.includes(parseInt(e))) ball++;
+  });
+
+  return { s: strike, b: ball };
+};
 
 const checkBtn = document.querySelector(".check-btn");
 checkBtn.addEventListener("click", () => {
-  console.log(document.querySelector("input").value);
-});
+  const inputNum = document.querySelector("input").value;
+  const result = checkNum(randomNum, inputNum);
+  const failMent = document.querySelector(".ment--fail");
 
-console.log(inputNum);
+  if (result.s === 3) {
+    failMent.style.display = "none";
+    document.querySelector(".ment--success").style.display = "block";
+  } else {
+    failMent.style.display = "block";
+    failMent.textContent = `힌트: 
+    ${result.b > 0 ? result.b + "볼" : ""} 
+    ${result.s > 0 ? result.s + "스트라이크" : ""}
+    ${result.b === 0 && result.s === 0 ? "낫싱" : ""}
+    `;
+  }
+});
