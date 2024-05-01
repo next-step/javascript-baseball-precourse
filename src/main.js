0051e651e0d8d98e2cbf68d9907ecbb0ce6ed9b9
minji2219 <200146@jnu.ac.kr>
2024-05-01 18:55:32 +0900

    랜덤한 상대방 숫자 생성
    *서로 다른 세자리 수
    *100의자리 숫자는 0이 될 수 없음

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const randomProduce = () => {
+  let firstNum = Math.floor(Math.random() * 10);
+  //첫번째 숫자가 0이 나오지 않을 때까지 실행
+  while (firstNum === 0) {
+    firstNum = Math.floor(Math.random() * 10);
+  }
+  let secondNum = Math.floor(Math.random() * 10);
+  while (firstNum === secondNum) {
+    secondNum = Math.floor(Math.random() * 10);
+  }
+  let thirdNum = Math.floor(Math.random() * 10);
+  while (firstNum === thirdNum || secondNum === thirdNum) {
+    thirdNum = Math.floor(Math.random() * 10);
+  }
+  return firstNum * 100 + secondNum * 10 + thirdNum * 1;
+};
+
+console.log(randomProduce());
