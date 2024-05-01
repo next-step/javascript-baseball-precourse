1a1a24979788b0ba2db3e981aabde1348f04d30c
geongyu09 <geongyu09@gmail.com>
2024-05-01 17:59:04 +0900

    feat: 정답 생성 로직 작성 (main.js)
    랜덤한 한자리 수를 생성하는 함수 getRandomNumber 추가
    겹치지 않도록 ANSWER_MAX_LENGTH 길이만큼 랜덤한 수를 만드는 makeAnswer 함수 추가
     - 함수의 반환값은 ANSWER_MAX_LENGTH 길이의 배열

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
-function init() {}
+const ANSWER_MAX_LENGTH = 3;
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10);
+}
+
+function makeAnswer() {
+  const answer = [];
+  while (answer.length < ANSWER_MAX_LENGTH) {
+    const number = getRandomNumber();
+    if (!answer.includes(number)) {
+      answer.push(number);
+    }
+  }
+  return answer;
+}
+
+function init() {
+  const answer = makeAnswer();
+}
 init();
