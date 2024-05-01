d6118c73145904b18469e81c2962c5bda4359e11
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-05-01 21:56:43 +0900

    feat: add hasDuplicateDigits function

--- src/utils/validators/validateBaseballUserInput.js
+++ src/utils/validators/validateBaseballUserInput.js
@@ -10,8 +10,14 @@ function isInRange(input) {
   return input.length === NUMBER.LENGTH;
 }
 
+function hasDuplicateDigits(input) {
+  const digits = input.toString().split('');
+  return new Set(digits).size !== digits.length;
+}
+
 export default function validateBaseballUserInput(input) {
   if (!isNumber(input)) return false;
   if (!isInRange(input)) return false;
+  if (hasDuplicateDigits(input)) return false;
   return true;
 }
