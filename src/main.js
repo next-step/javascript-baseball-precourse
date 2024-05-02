bd58ea489446e62d85e0b178d1c30dbf41fd7a7a
Hyoeunkh <lhe4528@gmail.com>
2024-05-02 19:03:43 +0900

    feat:틀렸을 경우, 볼 or 스트라이크 판별 및 결과 확인

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ function checkNum() {
 	const inputNum = document.querySelector('.search input').value;
 	var checkVal = checkValue(inputNum);
 	if (checkVal) {
-		//strike 판별 로직
+		ballOrStrike (inputNum)
 	}
 	else {
 		document.querySelector('.search input').focus();
@@ -38,5 +38,34 @@ function checkValue(inputNum) {
     return false;
   }
   return true;
+}
+
+// 숫자 입력 결과 확인
+function createResult(ball, strike) {
+  var resultText = '';
+  if (strike === 0 && ball >0)
+    resultText = `${ball}볼`
+  else if (strike > 0 && ball === 0)
+    resultText = `${strike}스트라이크`
+  else if (ball > 0 || strike > 0)
+    resultText = `${ball}볼 ${strike}스트라이크`
+  else 
+    resultText = '낫싱';
+  return resultText
+}
+// 볼 or 스트라이크 판별
+const correct = document.querySelector('.result-correct');
+function ballOrStrike (inputNum) {
+  var res ='';
+  var strike = 0;
+  var ball = 0;
+  inputNum.split('').forEach((num, index) => {
+    if (random.indexOf(num) === index)
+      strike++;
+    else if (random.includes(num))
+      ball++;
+  })
+  res = createResult(ball, strike)
+  console.log(document.querySelector('.result-hint').textContent = res);
 }
 
