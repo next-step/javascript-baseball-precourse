bb1d6fbfe6aee9385007008510f5fe6bf0f7c501
Dobbymin <gmin0701@knu.ac.kr>
2024-05-02 22:37:58 +0900

    feat(InputNumber.js): 입력받은 숫자 예외처리 후 겡미 진행 기능 구현

--- src/modules/InputNumber.js
+++ src/modules/InputNumber.js
@@ -1,4 +1,5 @@
 import { CheckNumber } from './CheckNumber.js';
+import { ErrorInput } from './ErrorInput.js';
 
 export let userInput = '';
 
@@ -8,20 +9,11 @@ export const InputNumber = (event) => {
 	const input = document.getElementById('submit-input');
 	userInput = input.value;
 
-	const digits = [];
-	digits.push(userInput.split(''));
-
-	// // 숫자를 유사배열객체인 문자열로 변경
-	// const str_input = String(userInput);
-
-	// // 배열의 모든 element를 숫자로 변경
-	// const mapfn = (element) => Number(element);
-
-	// // 문자열을 배열로 변경 - split()
-	// // 배열의 모든 element를 숫자로 변경 - map()
-	// arr_input = str_input.split('').map(mapfn);
-	// console.log(digits);
-	CheckNumber();
+	if (ErrorInput(userInput) === false) {
+		userInput = '';
+	} else {
+		CheckNumber();
+	}
 };
 
 // console.log(userNumber);
