de16fa952fe4153d16bddfa9c96851c403b19128
loveydev <tjgus9650@gmail.com>
2024-05-03 03:49:43 +0900

    랜덤 숫자 생성 함수 구현

--- src/randomGenerator.js
+++ src/randomGenerator.js
@@ -1,6 +1,6 @@
 export let answer = [];
 
-// 상대방이 1~9 사이의 숫자 중 중복 없이 3개를 선택하는 함수
+// 중복 없이 랜덤한 숫자 3개를 생성하는 함수
 export const randomNumberGenerate = () => {
   const nums = [1,2,3,4,5,6,7,8,9];
   answer.length = 0;
