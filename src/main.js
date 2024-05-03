452e5cff406e6417f35ff8b1d3982b624fa7c2b4
남소은 <namso-eun@namso-eun-ui-MacBookPro.local>
2024-05-03 12:58:47 +0900

    feat:사용자로부터 입력받기

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
 class App {
     getRandomNumber() {
-        const computer = [];
-        while (computer.length < 3) {
-            const number = Random.pickNumberInRange(1, 9);
-            if (!computer.includes(number)){
-                computer.push(number);
+        const numbers = [];
+        while (numbers.length < 3) {
+            const number = Math.floor(Math.random() * 9) + 1;
+            if (!numbers.includes(number)){
+                numbers.push(number);
             }
         }
-        return computer;
+        return numbers.join('');
     }
 }
+
+document.getElementById("checkButton").addEventListener("click", function() {
+    // App 클래스의 인스턴스 생성
+    const app = new App();
+
+    const randomNumber = app.getRandomNumber();
+    // 입력값 가져오기
+    let userInput = document.getElementById("userInput").value;
+
+    console.log(randomNumber);
+    console.log(userInput);
+
+});
 
 export default App;
