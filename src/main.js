f419d67c9807a1cef3bde2a5d5425a186df9677a
KimJi-An <j0012a@naver.com>
2024-05-04 00:15:57 +0900

    feat: 결과 출력 기능 구현

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+const computerNumbers = generateNumbers();
+
+const playerInput = document.querySelector("#playerInput");
+playerInput.oninput = validateInput;
+
+const submitBtn = document.querySelector("#submit");
+submitBtn.onclick = displayResult;
+
+const results = document.querySelector(".results");
+results.style.display = "none";
+
+const resultsList = document.querySelector(".results-list");
+
 // 컴퓨터가 임의의 수 3개 선택
 function generateNumbers() {
   let numbers = [];
@@ -41,7 +54,48 @@ function validateInput() {
   }
 }
 
-const computerNumbers = generateNumbers();
+// 플레이어가 입력한 숫자와 컴퓨터가 선택한 숫자 비교
+function calculateResult() {
+  let strikes = 0;
+  let balls = 0;
 
-const playerInput = document.querySelector("#playerInput");
-playerInput.oninput = validateInput;
+  for (let i = 0; i < 3; i++) {
+    const input = parseInt(playerInput.value[i]);
+    // 숫자와 위치 모두 일치(스트라이크)
+    if (input === computerNumbers[i]) {
+      strikes++;
+    }
+    // 숫자 일치, 위치 불일치(볼)
+    else if (computerNumbers.includes(input)) {
+      balls++;
+    }
+  }
+
+  let resultArr = [];
+
+  // 모두 불일치(낫싱)
+  if (strikes === 0 && balls === 0) {
+    return '낫싱';
+  }
+  else {
+    if (strikes > 0) {
+      resultArr.push(`${strikes}스트라이크`);
+    }
+    if (balls > 0) {
+      resultArr.push(`${balls}볼`);
+    }
+  }
+
+  return resultArr.join(' ');
+}
+
+// 화면에 결과 표시
+function displayResult() {
+  results.style.display = "block";
+  const result = calculateResult();
+  const resultItem = document.createElement('li');
+
+  resultItem.textContent = `${playerInput.value} : ${result}`;
+  resultsList.appendChild(resultItem);
+  playerInput.value = '';
+}
