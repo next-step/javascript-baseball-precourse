0f7ffa0e246ca32c725c59cc22fa5aa4f8713712
seung365 <bdh3659@naver.com>
2024-05-03 20:51:10 +0900

    chore : 문장 구조 수정

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import ckNum from './check_result.js';
 import random from './getRandom.js';
 import retry from './wrong_in.js';
 
-const buttonRefresh = document.getElementById('myButton');
-const checkButton = document.querySelector('input[name=check]');
+const buttonRefresh = document.getElementById('myButton')
+const checkButton = document.querySelector('input[name=check]')
 const correctNum = random()
-console.log(correctNum)
+//console.log(correctNum)
 
 checkButton.onclick = function() {
-  const inputText = document.querySelector('input[name=number]').value;
-  
-  const result = processInput(inputText);
 
+  const inputText = document.querySelector('input[name=number]').value
+  const result = processInput(inputText)
   const isError = retry(result)
 
   if(isError===1)
@@ -26,8 +25,8 @@ checkButton.onclick = function() {
   if(result===correctNum)
   { 
     // 결과를 'text-output' 요소에 출력
-    document.getElementById('text-output-1').innerHTML = "<b>🎉정답을 맞추셨습니다.🎉</b>";
-    document.getElementById('text-output-2').innerText = "게임을 새로 시작하겠습니까?";
+    document.getElementById('text-output-1').innerHTML = "<b>🎉정답을 맞추셨습니다.🎉</b>"
+    document.getElementById('text-output-2').innerText = "게임을 새로 시작하겠습니까?"
     buttonRefresh.style.display = 'block'; // 정답일 경우 myButton을 보이게 합니다.
   }
   else if(outList.ball ===0 && outList.strike ===0) //몇개 맞췄는지 보여주자.
@@ -49,7 +48,7 @@ function processInput(input) {
 }
 
 buttonRefresh.onclick =function() {
-  location.href = location.href;
+  location.href = location.href
 }
 
 
