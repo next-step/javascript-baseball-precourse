2f6ff8af25aa8a29b3e8861c89c633eadd2f9b3e
cla6shade <cla6shade@github.com>
2024-05-03 10:23:16 +0900

    feat(computer): add constructor for Computer
    Computer object generates an answer array when the instance is initialized

--- src/computer.js
+++ src/computer.js
@@ -1,4 +1,7 @@
 class Computer {
+  constructor() {
+    this.answer = this.generateAnswer();
+  }
   getShuffledArray() {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return arr.sort(() => Math.random() - 0.5);
