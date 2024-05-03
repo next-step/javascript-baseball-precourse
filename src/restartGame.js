49ce3d6e64914159a7e18a032e0a1725c232c6d3
loveydev <tjgus9650@gmail.com>
2024-05-04 01:28:17 +0900

    feat(restartGame.js) : 게임 재시작 기능 구현

---
+++ src/restartGame.js
@@ -0,0 +1,23 @@
+import { randomNumberGenerate } from "./randomGenerator.js";
+
+// 게임 재시작 버튼 활성화 함수
+export const showRestartGameButton = () => {
+  const restartButtonDisplay = document.getElementById("restartButton");
+  restartButtonDisplay.style.display = "block";
+};
+
+// 게임 재시작 문구 표시 함수
+export const showRestartMessage = () => {
+  const message = document.getElementById("restartMessage");
+  message.style.display = "block";
+};
+
+// 게임 재시작 함수
+export const restartGame = () => {
+  randomNumberGenerate();
+  document.getElementById("inputBox").value = "";
+  document.getElementById("resultMessage").textContent = "";
+  document.getElementById("restartMessage").style.display = "none";
+  document.getElementById("restartButton").style.display = "none";
+  document.getElementById("quitButton").style.display = "none";
+};
