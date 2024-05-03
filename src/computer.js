8dd6c3c8e040c28a3545372051ff150f58c7c884
cla6shade <cla6shade@github.com>
2024-05-03 10:22:15 +0900

    feat(computer): add answer generator for computer module
    generateAnswer() returns array which contains three differnt number

---
+++ src/computer.js
@@ -0,0 +1,11 @@
+class Computer {
+  getShuffledArray() {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return arr.sort(() => Math.random() - 0.5);
+  }
+  generateAnswer() {
+    const arr = this.getShuffledArray();
+    return arr.slice(0, 3);
+  }
+}
+export default Computer;
