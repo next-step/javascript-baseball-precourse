47dff9b1192f8726e7d1537576416d785e30f550
JeongSeoyun <xeonj@naver.com>
2024-05-03 19:34:05 +0900

    fix: 난수 범위 수정

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 const set = []; // 랜덤 난수 담을 배열
+let input = document.getElementById('input').value;
 
 while (set.length < 3) {
-  const rand = Math.floor(Math.random() * 10) + 1;
+  const rand = Math.floor(Math.random() * 9) + 1;
   if (!set.includes(rand)) {
     set.push(rand);
   }
 }
 console.log(set);
+
+function check() {
+  console.log(input);
+}
