ce6fc30a72b925f4fe94b0ec17c0a0ae266dc353
JeongSeoyun <xeonj@naver.com>
2024-05-04 12:37:56 +0900

    feat: 사용자 입력 오류시 alert() 처리

--- src/main.js
+++ src/main.js
@@ -16,4 +16,25 @@ function check() {
   let inputArr = input.toString().split('').map(Number);
   console.log(input);
   console.log(inputArr);
+
+  if (checkValid(inputArr)) {
+    console.log("유효한 입력")
+  }
+}
+
+function checkValid(inputArr) {
+  // 입력 숫자가 3개 맞는지 검사
+  if (inputArr.length !== 3) {
+    alert("숫자를 3개 입력해주세요");
+    return false;
+  }
+
+  // 중복 숫자 검사
+  let inputSet = new Set(inputArr);
+  if(inputSet.size !== 3) {
+    alert("1부터 9까지의 숫자를 중복 없이 3개 입력해주세요");
+    return false;
+  }
+
+  return true;
 }
