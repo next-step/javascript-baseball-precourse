b4c8741c5d002c56998c69ddfb23a4de05fdd779
seongikx <tjddlr3512@pusan.ac.kr>
2024-05-05 00:37:28 +0900

    feat : pc 정답 생성 기능 구현

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// pc - 정답 만들기
+let ans = generateAnswer();
+
+// pc - 정답 생성 함수
+function generateAnswer() {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let answerArray = [];
+
+  for (let i = 0; i < 3; i++) {
+    let index = Math.floor(Math.random() * numbers.length);
+    answerArray.push(numbers[index]);
+    numbers.splice(index, 1);
+  }
+  return answerArray;
+}
