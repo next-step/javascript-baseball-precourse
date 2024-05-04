83500e912622dec6247a8b8f56beb95bcaff3101
JeongSeoyun <xeonj@naver.com>
2024-05-04 13:40:53 +0900

    feat: 결과 판별 및 출력, 재시작 할 수 있도록 코드 구조 변경

--- src/main.js
+++ src/main.js
@@ -1,26 +1,57 @@
-const set = []; // 랜덤 난수 담을 배열
-
-while (set.length < 3) {
-  const rand = Math.floor(Math.random() * 9) + 1;
-  if (!set.includes(rand)) {
-    set.push(rand);
-  }
-}
-console.log(set);
-
+let input = document.getElementById('input');
 const checkButton = document.getElementById('check');
 checkButton.addEventListener('click', check);
+const result = document.getElementById('result');
+const restartQ = document.getElementById('restartQ');
+const restartBtn = document.getElementById('restartBtn');
+restartBtn.addEventListener('click', setting);
+
+let set;
+setting();
+
+// 게임 초기화 및 시작 함수
+function setting() {
+  set = []; // 랜덤 난수 담을 배열
+
+  while (set.length < 3) {
+    const rand = Math.floor(Math.random() * 9) + 1;
+    if (!set.includes(rand)) {
+      set.push(rand);
+    }
+  }
+  console.log(set);
+
+  result.textContent = '';
+  restartQ.style.display = 'none';
+  input.value = '';
+}
+
 
 function check() {
-  let input = document.getElementById('input').value;
-  let inputArr = input.toString().split('').map(Number);
-  console.log(input);
+  
+  let inputValue = input.value;
+  let inputArr = inputValue.toString().split('').map(Number);
+  console.log(inputValue);
   console.log(inputArr);
 
   if (checkValid(inputArr)) {
     console.log("유효한 입력")
+
+    // 스트라이크, 볼, 낫싱 체크
+    const { strike, ball, nothing } = checkResult(inputArr, set);
+    console.log(strike, "스트라이크 ", ball, "볼 ", nothing, "낫싱");
+
+    // 정답 처리 (3 스트라이크)
+    if (strike === 3) {
+      result.textContent = "🎉 정답을 맞추셨습니다 🎉"
+      restartQ.style.display = 'block';
+    } else {
+      result.textContent = `${strike} 스트라이크 ${ball} 볼 ${nothing} 낫싱`;
+    }
   }
+
 }
+
 
 function checkValid(inputArr) {
   // 입력 숫자가 3개 맞는지 검사
@@ -37,4 +68,24 @@ function checkValid(inputArr) {
   }
 
   return true;
+}
+
+
+function checkResult(inputArr, set) {
+  let strike = 0;
+  let ball = 0;
+  let nothing = 0;
+
+  for (let i=0; i<3; i++) {
+    if (inputArr[i] === set[i]) {
+      strike++;
+    } else if (set.includes(inputArr[i])) {
+      ball++;
+    } else {
+      nothing++;
+    }
+  }
+
+  return { strike, ball, nothing };
+
 }
