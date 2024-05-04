81cdfb9f3da6d9c424460e739ae4f3f6c5d23195
DevXuan <pourding@naver.com>
2024-05-05 04:54:19 +0900

    Feat(inputUserNumber): 사용자로부터 값을 입력받는 기능 + 예외처리

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export * from "./createRandomNumber";
+export * from "./inputUserNumber";
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createRandomNumber } from "./components";
+import { createRandomNumber, inputUserNumber } from "./components";
 
-let computerNumber = createRandomNumber();
-console.log(computerNumber);
+const main = async () => {
+  let computerNumber = createRandomNumber();
+  console.log(computerNumber);
+  let restart = true;
+
+  const submitButton = document.getElementById("submit");
+
+  submitButton.addEventListener("click", async () => {
+    event.preventDefault();
+    let userNumbers = await inputUserNumber();
+    console.log(userNumbers);
+  });
+};
+
+main();
