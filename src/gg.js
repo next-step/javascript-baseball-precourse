9687d4689ee5d396c891f7adda34e0c8a6e31569
joshuadesu <meron_pan@kakao.com>
2024-05-05 14:23:15 +0900

    Feat: Add 종료 버튼 클릭 시 pitch(게임 플레이)버튼 무효화

--- src/gg.js
+++ src/gg.js
@@ -1,10 +1,17 @@
 // 게임 재시작 함수
 export const reGame = document.querySelector(".restart");
+export const quitGame = document.querySelector(".quit");
 
 export const goodGame = () => {
   // 버튼 누를 시 페이지 새로고침
   reGame.addEventListener("click", reload);
   function reload() {
     location.reload(true);
+  }
+  // 버튼 누를 시 페이지 무효화
+  quitGame.addEventListener("click", quit);
+  function quit() {
+    const kill = document.querySelector("button");
+    kill.disabled = true;
   }
 };
