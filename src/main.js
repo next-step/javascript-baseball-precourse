5a989a063cb0e2d28694cf0c3bf2c5459fc933e8
leejongil1046 <lejongil1046@gmail.com>
2024-05-06 01:45:05 +0900

    feat: add a function for validating user numbers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createRandomNumbers } from "./createRandomNumbers";
+import { validateUserNumbers } from "./validateUserNumbers";
 
 let randomNumbers = createRandomNumbers();
+let userNumbers;
 
-console.log(randomNumbers);
+const inputEl = document.querySelector("#number-input");
+const submitEl = document.querySelector("#number-submit");
+
+inputEl.addEventListener("input", (event) => {
+  userNumbers = event.target.value;
+});
+
+submitEl.addEventListener("click", () => {
+  console.log("randomNumbers =", randomNumbers);
+  console.log("userNumbers =", userNumbers);
+  console.log(validateUserNumbers(userNumbers));
+});
