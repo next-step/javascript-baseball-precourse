3de83c72a6a8f09d6246f6fd0ec222780a3302c8
tteokbokki-master <0jin.git@gmail.com>
2024-05-05 12:39:00 +0900

    feat: 컴퓨터와 사용자 비교 구현

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { computerNumber } from "./computerNumber.js";
 import { userNumber } from './userNumber.js';
+import { compareNumber } from './compareNumber.js';
 
 const userNumberInput = document.querySelector('.userNumberInput');
 const userNumberBtn = document.querySelector('.userNumberBtn');
 
 const computerNum = computerNumber();
+console.log(computerNum);
 
 userNumberBtn.addEventListener('click', () => {
   let userNum = userNumber(userNumberInput.value);
   if (userNum === null) {
     return;
   }
-  console.log(userNum);
+  let {strike,ball} = compareNumber(userNum, computerNum);
+  console.log(strike[0],ball[0]);
 })
+
