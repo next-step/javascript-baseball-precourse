d2d9d62208916dc95e8e66247e42665e09266fbd
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-05-06 07:04:02 +0900

    feat(main): add input feature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+const input = document.querySelector('.input input');
+const inputButton = document.querySelector('.input button');
+
+let gameOver = false;
 let randNumbers = getRandomNumbers();
+
+inputButton.addEventListener('click', () => {
+  // 게임 종료 시, 버튼 무효화
+  if (gameOver) return;
+
+  // 사용자 입력 받아오기
+  const userInput = input.value;
+  const userNumbers = userInput.split('').map(Number);
+});
