275be91b364627be7431e9e20c332c5256ce486c
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-05-05 19:22:46 +0900

    feat(기능 3) 기능 3 구현

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import randomnum from './random'
 let numbers = []
 
 randomnum()
+
+const pushB = document.querySelector('#pushBtn')
+pushB.addEventListener('click',() =>{
+    let userinputEl = document.querySelector('#userInput').value
+})
