fdb9f22020296c12c8e29b0586b8ac8e1a751ec3
wjdsbs <wjdsbss@gmail.com>
2024-05-06 06:42:33 +0900

    feat : 답 입력받는 기능 및 에러 메시지 출력 기능 추가

--- src/main.js
+++ src/main.js
@@ -9,4 +9,19 @@ function playGame() {
     console.log(answer)
 }
 
-// console.group(checkNumbers(answer))
+// 확인 버튼
+const sendBtn = document.querySelector('.send-btn')
+sendBtn.addEventListener('click', () => {
+    let guessNum = document.querySelector('.input-box').value.split('').map(Number)
+    console.log("inputValue", guessNum)
+    console.log("answer", answer)
+
+    let valid = checkNumbers(guessNum)
+    console.log("valid", valid)
+    if (valid === true) {
+    } else {
+        alert("잘못된 값이 입력되었어요. 다시 입력해주세요.");
+    }
+});
+
+// console.log(checkNumbers(guessNum))
