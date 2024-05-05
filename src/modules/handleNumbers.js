579d105929bc3ee6a3f8f1071965fb3849d624a9
yeahzee0421 <ydw08250421@gmail.com>
2024-05-05 18:43:46 +0900

    feat: add alert playerNumber isValid feature

--- src/modules/handleNumbers.js
+++ src/modules/handleNumbers.js
@@ -10,3 +10,18 @@ function createRandomNumbers() {
   }
   return computerNumbers;
 }
+
+function getPlayerNumbers() {
+  const playerNumbers = document
+    .getElementById("playerInput")
+    .value.split("")
+    .map(Number); //map형식
+  //입력이 유효한지 판단 -> 3자리 수가 아니거나 중복된 숫자가 있는 경우 alert
+  if (playerNumbers.length !== 3 || new Set(playerNumbers).size !== 3) {
+    alert("잘못된 입력입니다. 1에서 9까지 서로 다른 3개의 숫자를 입력하세요.");
+    return null;
+  }
+  return playerNumbers;
+}
+
+export { createRandomNumbers, getPlayerNumbers };
