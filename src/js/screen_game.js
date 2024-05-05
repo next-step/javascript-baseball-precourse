6b6700489d0b9ab497291565caafaae4bbc718eb
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-05-05 10:31:24 +0900

    feat : 랜덤 3자리수 생성

--- src/js/screen_game.js
+++ src/js/screen_game.js
@@ -1,3 +1,18 @@
+const usage_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let _answer = [];
+let _problem = [];
+let _logs = [];
+let _require_number = [];
+let _exclude_number = [];
+
+const generateProblem = () => {
+  _answer = [];
+  _problem = usage_numbers.sort(() => Math.random() - 0.5).slice(0, 4);
+  _logs = [];
+  _require_number = [];
+  _exclude_number = [];
+};
+
 const init_screen_game = () => {
   const screen_game = document.getElementById("screen--game");
   screen_game.style.display = "none";
@@ -6,6 +21,8 @@ const init_screen_game = () => {
 const onStartGame = () => {
   const screen_game = document.getElementById("screen--game");
   screen_game.style.display = "flex";
+
+  generateProblem();
 };
 
 export { init_screen_game, onStartGame };
