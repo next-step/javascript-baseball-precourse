e8b1a8fe631a885eca6da3256511150f3c96e6c6
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-05-06 00:25:28 +0900

    feat(기능 6) 기능 6 구현

---
+++ src/error.js
@@ -0,0 +1,21 @@
+export default (userinputElValue) => {
+    let userInputList = userinputElValue.split('')
+    //사용자 입력 문자 길이가 3이 아닐 시 false
+    if(userInputList.length !== 3){
+        return false
+    }
+    //사용자가 1~9가 아닌 다른 문자를 입력시 false
+    for(let i = 0; i< 3; i+=1){
+        if(!/[1-9]/.test(userInputList[i])){
+            return false
+        }
+    }
+    
+    let list = []
+    list.push(userInputList[0])
+    for(let i = 1 ; i<3 ; i+=1){
+        if(list.includes(userInputList[i]))
+            return false
+    }
+    return true
+}
