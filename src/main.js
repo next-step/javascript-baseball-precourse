456dede71320e56a653f311e20830f9db72cb866
eunjin <eunjin0321@naver.com>
2024-05-05 18:58:03 +0900

    사용자 입력값 확인 코드

--- src/main.js
+++ src/main.js
@@ -6,3 +6,23 @@ let result = getRandom()
 // 콘솔창에서 정답 확인 가능
 result = result.join('')
 console.log('정답:',result)
+
+// 사용자 입력을 받는 코드
+let input
+let count = 0
+let resultArr= []
+
+document.querySelector('#input_btn').addEventListener('click', function() {
+  input = document.getElementById('input_num').value
+  console.log(input)
+  const Inputset = new Set(input);
+
+  // 잘못된 입력에 대한 오류 처리
+  if (Inputset.size != 3) {
+    alert('잘못 입력된 값');
+    document.getElementById('input_num').value = '';
+    return -1;
+  }
+  // 제대로 입력 됐다면, 게임 시작
+    game(input);
+  });
