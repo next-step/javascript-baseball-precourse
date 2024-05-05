de81b791588f9b96de72db20fae8ac7e72e77a7f
leejongil1046 <lejongil1046@gmail.com>
2024-05-06 03:08:48 +0900

    refact: organize functions to initialize game

--- src/main.js
+++ src/main.js
@@ -2,31 +2,44 @@ import { createRandomNumbers } from "./createRandomNumbers";
 import { validateUserNumbers } from "./validateUserNumbers";
 import { getResult } from "./getResult";
 
-let randomNumbers = createRandomNumbers();
-let userNumbers;
+let randomNumbers;
+let userNumbers = "";
 
-const inputEl = document.querySelector("#number-input");
-const submitEl = document.querySelector("#number-submit");
-const resultContainerEl = document.querySelector("#result-container");
-const resultTextEl = document.querySelector("#result-text");
-const restartContainerEl = document.querySelector("#restart-container");
-const restartBtnEl = document.querySelector("#btn-restart");
+let userInputEl,
+  submitBtnEl,
+  resultContainerEl,
+  resultTextEl,
+  restartContainerEl,
+  restartBtnEl;
 
-inputEl.addEventListener("input", (event) => {
-  userNumbers = event.target.value;
-});
+function initializeGame() {
+  randomNumbers = createRandomNumbers();
 
-submitEl.addEventListener("click", () => {
-  if (!validateUserNumbers(userNumbers)) {
-    alert(
-      "잘못된 입력값입니다. 1에서 9까지의 중복되지 않는 세 자리 수를 입력하세요."
-    );
-    inputEl.focus();
-  } else {
-    const { resultText, isCorrect } = getResult(randomNumbers, userNumbers);
-    displayResult(resultText, isCorrect);
-  }
-});
+  userInputEl = document.querySelector("#number-input");
+  submitBtnEl = document.querySelector("#number-submit");
+  resultContainerEl = document.querySelector("#result-container");
+  resultTextEl = document.querySelector("#result-text");
+  restartContainerEl = document.querySelector("#restart-container");
+  restartBtnEl = document.querySelector("#btn-restart");
+
+  userInputEl.addEventListener("input", (event) => {
+    userNumbers = event.target.value;
+  });
+
+  submitBtnEl.addEventListener("click", () => {
+    if (!validateUserNumbers(userNumbers)) {
+      alert(
+        "잘못된 입력값입니다. 1에서 9까지의 중복되지 않는 세 자리 수를 입력하세요."
+      );
+      userInputEl.focus();
+    } else {
+      const { resultText, isCorrect } = getResult(randomNumbers, userNumbers);
+      displayResult(resultText, isCorrect);
+    }
+  });
+
+  restartBtnEl.addEventListener("click", restartGame);
+}
 
 function displayResult(resultText, isCorrect) {
   resultContainerEl.style.display = "block";
@@ -34,11 +47,11 @@ function displayResult(resultText, isCorrect) {
 
   if (isCorrect) {
     restartContainerEl.style.display = "block";
-    restartBtnEl.removeEventListener("click", restartGame);
-    restartBtnEl.addEventListener("click", restartGame);
   }
 }
 
 function restartGame() {
   location.reload();
 }
+
+initializeGame();
