f135fd5d1b058004d6e398027b26d9d71f37ecd4
leejongil1046 <lejongil1046@gmail.com>
2024-05-06 01:54:38 +0900

    feat: add alert for invalid inputs

--- src/main.js
+++ src/main.js
@@ -15,4 +15,10 @@ submitEl.addEventListener("click", () => {
   console.log("randomNumbers =", randomNumbers);
   console.log("userNumbers =", userNumbers);
   console.log(validateUserNumbers(userNumbers));
+  if (!validateUserNumbers(userNumbers)) {
+    alert(
+      "잘못된 입력값입니다. 1에서 9까지의 중복되지 않는 세 자리 수를 입력하세요."
+    );
+    inputEl.focus();
+  }
 });
