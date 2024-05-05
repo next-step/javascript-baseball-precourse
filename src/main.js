8669dd4afe81da5f2fadd31af4ec5b6b1ab1ce8f
yeahzee0421 <ydw08250421@gmail.com>
2024-05-05 17:14:47 +0900

    feat: add create random-number feature

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import createRandomNumber from "./createRandomNumber";
-
-const computerNum = createRandomNumber();
-
-console.log(computerNum);
+function createRandomNumber() {
+  const numbers = []; //숫자를 담을 배열 생성
+  while (numbers.length < 3) {
+    const randomNumber = Math.floor(Math.random() * 9) + 1;
+    if (!numbers.includes(randomNumber)) {
+      //해당 숫자가 없으면
+      numbers.push(randomNumber); //추가해준다
+    }
+  }
+  return numbers;
+}
