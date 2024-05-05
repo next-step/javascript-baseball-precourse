9ec4a75f08d4f376d2c1d25aebb521eb33577f00
Eric <real.dassy@gmail.com>
2024-05-06 02:35:14 +0900

    refactor: 모듈화로 인한 코드 이동
    main.js에 있던 코드들을 모듈화 했습니다.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,21 @@
-import { Game } from "./game.js";
-
-const inputEl = document.querySelector(".input #input__textfield");
-const inputBtnEl = document.querySelector(".input #input__btn");
-const resultTxtEl = document.querySelector(".result .result__text");
-const restartBtnEl = document.querySelector(".result .result__btn");
-const restartMsgEl = document.querySelector(".result .result__restartMessage");
+import { Game } from "./games/game.js";
+import { inputTextFieldEl, inputBtnEl, restartBtnEl, finishBtnEl, titleStartBtnEl } from "./components/elements.js";
+import {
+  initGameInstance,
+  handleInputTextFieldKeyPress,
+  handleInputBtnClick,
+  handleRestartBtnClick,
+  handleTitleStartBtnClick,
+  handleDOMContentLoaded,
+  handleFinishBtnClick,
+} from "./games/eventHandlers.js";
 
 const game = new Game();
-console.log(game.answer);
 
-inputBtnEl.addEventListener("click", function () {
-  game.compareResult(inputEl);
-  resultTxtEl.innerText = game.getResult();
-
-  if (game.isGameSet) {
-    restartMsgEl.innerText = "게임을 새로 시작하시겠습니까?";
-    restartBtnEl.type = "button";
-  }
-});
-
-restartBtnEl.addEventListener("click", function () {
-  game.restart(() => {
-    restartBtnEl.type = "hidden";
-    resultTxtEl.innerText = game.getResult();
-    inputEl.value = "";
-    restartMsgEl.innerText = "";
-  });
-});
+initGameInstance(game);
+inputTextFieldEl.addEventListener("keypress", handleInputTextFieldKeyPress);
+inputBtnEl.addEventListener("click", handleInputBtnClick);
+restartBtnEl.addEventListener("click", handleRestartBtnClick);
+titleStartBtnEl.addEventListener("click", handleTitleStartBtnClick);
+document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
+finishBtnEl.addEventListener("click", handleFinishBtnClick);
