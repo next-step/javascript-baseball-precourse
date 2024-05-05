078f3165b984a1aa87278a666f82b66291fc610c
2021048695 <hanyanging@hanyang.ac.kr>
2024-05-06 05:45:09 +0900

    입력값 유효성 체크

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ function processInput(input) {
   return input.split('').map(Number);
 }
 
+// 입력값 유효성 체크
+function checkInput(input) {
+  if (input.length !== 3) {
+    return false;
+  }
+  const unums = new Set(input);
+  return unums.size === 3;
+}
+
