14741ae22fb0f928ca7457000528fa0652b357a3
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-05-05 16:57:45 +0900

    feat : 정답 입력창

---
+++ src/js/answer_input.js
@@ -0,0 +1,66 @@
+let _answer = [];
+let _answer_inputs;
+let _onEndInput;
+
+const validateAnswerUnit = (unit) => {
+  const regexp = /^[1-9]$/;
+  return regexp.test(unit);
+};
+
+const fillAnswerUnit = (unit) => {
+  if (_answer.length < 2) {
+    _answer.push(unit);
+    _answer_inputs[_answer.length].focus();
+  } else {
+    _answer.push(unit);
+    _onEndInput(_answer);
+  }
+};
+
+const handleAnswerUnitKeydown = (e) => {
+  if (e.key === "Backspace") {
+    _answer.pop();
+    _answer_inputs[_answer.length].focus();
+  }
+};
+
+const handleAnswerUnitInput = (e) => {
+  const value = e.target.value;
+  if (value === "") {
+    return;
+  } else if (!validateAnswerUnit(value)) {
+    alert("1부터 9까지의 숫자만 입력할 수 있습니다.");
+    e.target.value = "";
+  } else if (_answer.includes(value)) {
+    alert("중복된 숫자는 입력할 수 없습니다.");
+    e.target.value = "";
+  } else {
+    fillAnswerUnit(value);
+  }
+};
+
+const handleAnswerReset = () => {
+  _answer = [];
+  for (const input of _answer_inputs) {
+    input.value = "";
+  }
+  _answer_inputs[0].focus();
+};
+
+const init_answer_input = (answer_input, onEndInput) => {
+  _answer_inputs = answer_input.children;
+
+  for (const input of _answer_inputs) {
+    input.addEventListener("input", handleAnswerUnitInput);
+    input.addEventListener("keydown", handleAnswerUnitKeydown);
+    input.addEventListener("click", handleAnswerReset);
+  }
+
+  _onEndInput = onEndInput;
+};
+
+const start_input = () => {
+  handleAnswerReset();
+};
+
+export { init_answer_input, start_input };
--- src/js/screen_game.js
+++ src/js/screen_game.js
@@ -1,3 +1,5 @@
+import { init_answer_input, start_input } from "./answer_input";
+
 const usage_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let _answer = [];
 let _problem = [];
@@ -16,6 +18,9 @@ const generateProblem = () => {
 const init_screen_game = () => {
   const screen_game = document.getElementById("screen--game");
   screen_game.style.display = "none";
+
+  const answer_input = document.getElementById("screen--game__input");
+  init_answer_input(answer_input, () => {});
 };
 
 const onStartGame = () => {
@@ -23,6 +28,8 @@ const onStartGame = () => {
   screen_game.style.display = "flex";
 
   generateProblem();
+
+  start_input();
 };
 
 export { init_screen_game, onStartGame };
