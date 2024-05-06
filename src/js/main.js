e56e57b8f551e1a42737e5e3a9836782123b609b
daeun0726 <delee2008@naver.com>
2024-05-06 19:02:03 +0900

    유효성 검사 수정

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const startBtn = document.querySelector('.btn--start');
 const restartBtn = document.querySelector('.btn--restart');
+const answerInput = document.getElementById('input--answer');
 const answerBtn = document.querySelector('.btn--answer');
 const gameStart = document.querySelector('.game-start');
 const gameRestart = document.querySelector('.game-restart');
@@ -16,6 +17,23 @@ function getRandomNumbers() {
     return numbers;
 }
 
+function isValid(input){
+    let inputArray = Array.from(new Set(input.split('').map(Number)));
+    if(inputArray.length === 3 && input.length === 3){
+        showResult(input,randomNumbers);
+    }else{
+        alert("잘못된 입력 형식입니다!");
+    }
+}
+
+
+
+function showResult(input, randomNumbers){
+
+}
+
+
+
 let randomNumbers;
 
 startBtn.addEventListener('click', function(){
@@ -29,4 +47,11 @@ restartBtn.addEventListener('click', function(){
     gameRestart.classList.add('hide');
     gameStart.classList.remove('hide');
     randomNumbers = getRandomNumbers();
+});
+
+answerBtn.addEventListener('click', function() {
+    const answerValue = answerInput.value;
+    isValid(answerValue);
+    gameRestart.classList.remove('hide');
+    gameStart.classList.add('hide');
 });
