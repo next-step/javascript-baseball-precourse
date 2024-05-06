70abef632ada865cff31fe633aa5ea1d6c2162b1
joojjang <itchu999@naver.com>
2024-05-06 23:18:10 +0900

    [Feat] calculate result

--- src/main.js
+++ src/main.js
@@ -12,12 +12,13 @@ function generateRandomNumber() {
   return digits;
 }
 
-function handleInput(userInput) {
+function handleInput(randomNumber, userInput) {
   const userInputValue = userInput.value;
 
   if (validateInput(userInputValue)) {
-    // 정답인지 판단 과정 넣기
-    console.log("valid");
+    const result = calculateResult(randomNumber, userInputValue);
+    // console.log(result);
+    return result; // { strikes, balls }
   } else {
     alert("잘못된 입력입니다. 다시 입력해주세요.");
     userInput.value = "";
@@ -28,22 +29,44 @@ function validateInput(input) {
   return /^[1-9]{3}$/.test(input) && new Set(input).size === 3;
 }
 
+function calculateResult(randomNumber, userInput) {
+  const randomDigits = randomNumber;
+  const userDigits = userInput.split("").map(Number);
+
+  let strikes = 0;
+  let balls = 0;
+
+  for (let i = 0; i < randomDigits.length; i++) {
+    if (randomDigits[i] === userDigits[i]) {
+      strikes++;
+    } else if (randomDigits.includes(userDigits[i])) {
+      balls++;
+    }
+  }
+
+  return { strikes, balls };
+}
+
 // 시작
 window.onload = function () {
   const randomNumber = generateRandomNumber();
-  //   console.log(randomNumber);
+  console.log("random number: ", randomNumber);
+  let strikes = 0;
+  let balls = 0;
 
   const inputButton = document.getElementById("inputbutton");
   const userInput = document.getElementById("userinput");
 
   inputButton.addEventListener("click", function () {
-    handleInput(userInput);
+    ({ strikes, balls } = handleInput(randomNumber, userInput));
+    console.log(strikes, ", ", balls);
   });
 
   // 키보드에서 엔터 키 입력
   userInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      handleInput(userInput);
+      ({ strikes, balls } = handleInput(randomNumber, userInput));
+      console.log(strikes, ", ", balls);
     }
   });
 };
