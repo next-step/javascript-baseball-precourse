280ebd5d5454a1a153dce06563e75891e4adedef
dsrud1218 <dmsrud566@naver.com>
2024-05-06 12:13:41 +0900

    수정

--- src/randomnums.js
+++ src/randomnums.js
@@ -1,12 +1,11 @@
 //랜덤숫자 
-function randomNum(){
-    let nums = [1,2,3,4,5,6,7,8,9];
-    let array=[];
-
-    for(let i=0;i<3;i++){
-        let index = Math.floor(Math.random()*nums.length);
-       array.push(nums[index]);
-        numbers.splice(idx,1)
+function RandomNumbers() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const n = Math.floor(Math.random() * 9) + 1;
+      if (!numbers.includes(n)) {
+        numbers.push(n);
+      }
     }
-    return result;
-}
+    return numbers.join(""); // 숫자 배열을 문자열로 반환
+  }
