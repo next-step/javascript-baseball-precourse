5993403fd4e78b1f5870cd2992c1ad1d6c91533b
joojjang <itchu999@naver.com>
2024-05-06 22:48:47 +0900

    [Feat] handle the input by enter key

--- src/main.js
+++ src/main.js
@@ -12,7 +12,23 @@ function generateRandomNumber() {
   return digits;
 }
 
+// 시작
 window.onload = function () {
   const randomNumber = generateRandomNumber();
-  console.log(randomNumber);
+  //   console.log(randomNumber);
+
+  const inputButton = document.getElementById("inputbutton");
+  inputButton.addEventListener("click", function () {
+    const userInput = document.getElementById("userinput").value;
+    console.log(userInput);
+  });
+
+  // 키보드에서 엔터 키 입력
+  const userInput = document.getElementById("userinput");
+  userInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const userInput = document.getElementById("userinput").value;
+      console.log(userInput);
+    }
+  });
 };
