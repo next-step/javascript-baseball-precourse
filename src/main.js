10b67cc79c1664bc1e522e33ab7c3ce6b53a7b89
unknown <kc21ynje@gmail.com>
2024-05-06 10:45:27 +0900

    fix(main): show hint - depth

--- src/main.js
+++ src/main.js
@@ -43,18 +43,23 @@ checkBtn.addEventListener('click', function () {
             restart.style.display = 'block'
         }
         else {
-            let content = ""
-
-            if(ball != 0)
-                content += ball+"볼 "
-            if(strike != 0)
-                content += strike+"스트라이크"
-            if(ball == 0 && strike == 0)
-                content = "낫싱"
-            result.textContent = content
+            result.textContent = makeHint()
         }
     }
 });
+
+function makeHint() {
+    let content = ""
+
+    if(ball != 0)
+        content += ball+"볼 "
+    if(strike != 0)
+        content += strike+"스트라이크"
+    if(ball == 0 && strike == 0)
+        content = "낫싱"
+
+    return content
+}
 
 function checkInput(answer) {
     if (answer.length != 3)
