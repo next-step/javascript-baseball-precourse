bfdb6c173f909bd5008e182190207fe0f544f4d7
rudtj <kyoungseo0327@gmail.com>
2024-05-06 12:49:36 +0900

    last

--- src/gameResult.js
+++ src/gameResult.js
@@ -1,23 +1,26 @@
 import { randomNum } from "./\brandomNum";
 
-let comValue = randomNum();
-
 export function gameResult(userInput) {
     let strike = 0;
     let ball = 0;
-    
+    let comValue = randomNum();
 
-    for (let i = 0; i < 3; i++) {
-        if (userInput[i] === comValue[i]) {
-            strike++;
-        } else {
-            const index = comValue.indexOf(userInput[i]);
-            if (index !== -1 && index !== i) {
-                // comValue 내에 userInput[i]가 존재하지만, 위치가 다를 경우
-                ball++;
-            }
+    // 볼 계산
+    for (let i = 0; i < userInput.length; i++) {
+        if (comValue.includes(userInput[i]) && userInput[i] !== comValue[i]) {
+            ball++;
         }
     }
+
+    // 스트라이크 계산
+    for (let i = 0; i < comValue.length; i++) {
+        if (userInput[i] === comValue[i]) {
+            strike++;
+            ball--; 
+        }
+    }
+
+    ball = ball < 0 ? 0 : ball;
 
     if (strike === 3) {
         return { message: '정답을 맞추셨습니다! 게임을 새로 시작하시겠습니까?', isCorrect: true };
--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     restartButton.addEventListener('click', () => window.location.reload());
   
-    let answer = randomNum();
-
+    let answer = randomNum(); 
+    
     confirmButton.addEventListener('click', () => {
       // 입력값에 대한 에러 체크
       const error = checkError(userInput.value);
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   
       // 게임 결과 확인
-      const result = gameResult(Number(userInput.value), Number(answer));
+      const result = gameResult(userInput.value.split(''), answer);
       alert(result.message); 
   
       // 정답을 맞췄을 경우, 게임 재시작 버튼을 화면에 추가
