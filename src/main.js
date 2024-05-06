f99240217623beb6b765a6804d57606d0e1e4862
joojjang <itchu999@naver.com>
2024-05-06 23:06:55 +0900

    [Feat] handle & validate the input

--- src/main.js
+++ src/main.js
@@ -12,23 +12,38 @@ function generateRandomNumber() {
   return digits;
 }
 
+function handleInput(userInput) {
+  const userInputValue = userInput.value;
+
+  if (validateInput(userInputValue)) {
+    // 정답인지 판단 과정 넣기
+    console.log("valid");
+  } else {
+    alert("잘못된 입력입니다. 다시 입력해주세요.");
+    userInput.value = "";
+  }
+}
+
+function validateInput(input) {
+  return /^[1-9]{3}$/.test(input) && new Set(input).size === 3;
+}
+
 // 시작
 window.onload = function () {
   const randomNumber = generateRandomNumber();
   //   console.log(randomNumber);
 
   const inputButton = document.getElementById("inputbutton");
+  const userInput = document.getElementById("userinput");
+
   inputButton.addEventListener("click", function () {
-    const userInput = document.getElementById("userinput").value;
-    console.log(userInput);
+    handleInput(userInput);
   });
 
   // 키보드에서 엔터 키 입력
-  const userInput = document.getElementById("userinput");
   userInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      const userInput = document.getElementById("userinput").value;
-      console.log(userInput);
+      handleInput(userInput);
     }
   });
 };
