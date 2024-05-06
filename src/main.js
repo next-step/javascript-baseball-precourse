b7e247f3de3e316046025d3bc6a7086a12856801
unknown <kc21ynje@gmail.com>
2024-05-06 10:48:32 +0900

    fix(main): fix(main): input check - enter 0

--- src/main.js
+++ src/main.js
@@ -66,6 +66,8 @@ function checkInput(answer) {
         return false
     if (answer[0] == answer[1] || answer[1] == answer[2] || answer[2] == answer[0])
         return false
+    if (answer[0] == 0 || answer[1] == 0 || answer[2] == 0)
+        return false
     return true
 }
 
