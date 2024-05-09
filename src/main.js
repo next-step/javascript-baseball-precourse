d4f4b3bd44144971a870530ea33249cd585bc7bc
Kuheewon <selly3109@naver.com>
2024-05-09 15:34:27 +0900

    feat. 사용자가 입력한 값

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 //컴퓨터가 입력한 값, 0~9까지 랜덤으로 3자리 수 생성
-let numbers = generatedRandomNumber();
+let numbers = generatedRandomNumber()
 
 function generatedRandomNumber() {
     let randomNumbers = []
     while(randomNumbers.length < 3) {
         const num = Math.floor(Math.random() * 10);
         if (!randomNumbers.includes(num))
-            randomNumbers.push(num);
+            randomNumbers.push(num)
     }
     return randomNumbers
 }
+
+//사용자가 입력한 값
+let answer;
+
+window.onload = function() {
+    const submitButton = document.getElementById('submitButton')
+    submitButton.addEventListener('click,' , () => {
+        answer = checkAnswer(answer)
+        if(answer) {
+            evaluateGuess(answer)
+        }
+    });
+};
+
 
 
