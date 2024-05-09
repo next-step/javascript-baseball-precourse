1e1b8f2ae6a866e2eb982f148f2e5318f0979131
Kuheewon <selly3109@naver.com>
2024-05-09 15:38:26 +0900

    feat 유효성 검사

--- src/main.js
+++ src/main.js
@@ -24,5 +24,23 @@ window.onload = function() {
     });
 };
 
+//유효성 검사
+function checkAnswer(answer) {
+    const userInput = document.getElementById('userInput').value
+    if (userInput.length !== 3) {
+        alert('3자리 수를 입력하세요.')
+        return
+    }
+    answer = userInput.split('').map(numbers)
+    if (hasDuplicates(answer)) {
+        alert('중복된 숫자를 포함할 수 없습니다.')
+        return;
+    }
+    return answer;
+}
 
+//중복 제거
+function hasDuplicates(array) {
+    return (new Set(array)).size !== array.length;
+}
 
