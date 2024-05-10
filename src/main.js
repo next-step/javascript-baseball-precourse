a6e3e445ac4a68398c1ba10434ff7f7e2a144a32
sondonghun97 <ehfk11115@gmail.com>
2024-05-10 15:51:34 +0900

    feat: 1~9까지 랜덤숫자생성

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function generateNumber() {
+    let numbers = '';
+
+    while (numbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * 9) + 1;
+      if (!numbers.includes(randomNumber)) {
+        numbers += randomNumber;
+      }
+    }
+
+    return numbers;
+  }
+
+
