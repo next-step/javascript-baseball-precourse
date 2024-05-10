f8db65cbefcd907463317e99b290db0ac73727e9
sondonghun97 <ehfk11115@gmail.com>
2024-05-10 16:54:57 +0900

    feat: 정답맞추기전까지 전에 생성된 정답이 유지되도록 수정

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ const resultEl = document.getElementById('result');
 submitBtn.addEventListener('click', displayResult);
 
 let answer = generateNumber();
-let strikes = 0;
-let balls = 0;
-
-
 
 function generateNumber() {
     let numbers = '';
@@ -21,38 +17,36 @@ function generateNumber() {
     return numbers;
 }
 
-
 function validateInput(inputValue) {
-  if (inputValue.length !== 3 || !/^[1-9]{3}$/.test(inputValue) || new Set(inputValue).size !== 3) {
-    alert('중복되지 않은 1~9까지의 숫자 3개를 입력하세요.');
-    return false;
-  }
+    if (inputValue.length !== 3 || !/^[1-9]{3}$/.test(inputValue) || new Set(inputValue).size !== 3) {
+        alert('중복되지 않은 1~9까지의 숫자 3개를 입력하세요.');
+        return false;
+    }
+    return true;
 }
 
-  strikes = 0;
-  balls = 0;
-  for (let i = 0; i < 3; i++) {
-    const input = parseInt(inputValue[i]);
-    if (input === parseInt(answer[i])) {
-      strikes++;
-    } else if (answer.includes(input.toString())) {
-      balls++;
+function displayResult() {
+    const inputValue = playerInput.value;
+    if (!validateInput(inputValue)) {
+        return;
     }
-  }
 
-  return true;
+    let strikes = 0;
+    let balls = 0;
+    for (let i = 0; i < 3; i++) {
+        const input = parseInt(inputValue[i]);
+        if (input === parseInt(answer[i])) {
+            strikes++;
+        } else if (answer.includes(input.toString())) {
+            balls++;
+        }
+    }
 
-
-  function displayResult() {
-  const inputValue = playerInput.value;
-  if (validateInput(inputValue)) {
     if (strikes === 3) {
-      resultEl.innerText = '🎉정답을 맞추셨습니다🎉';
+        resultEl.innerText = '🎉정답을 맞추셨습니다🎉';
     } else if (strikes === 0 && balls === 0) {
-      resultEl.innerText = '낫싱';
+        resultEl.innerText = '낫싱';
     } else {
-      resultEl.innerText = `${balls}볼 ${strikes}스트라이크`;
+        resultEl.innerText = `${balls}볼 ${strikes}스트라이크`;
     }
-  }
 }
-
