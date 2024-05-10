cc863b9078486496a533e3c3789010bd75aa070f
faddishcorn <juni129461@gmail.com>
2024-05-10 19:00:05 +0900

    feat: 정답 입력을 처리

--- src/main.js
+++ src/main.js
@@ -15,3 +15,23 @@ const $confirmButton = document.querySelector('#confirmButton');
 //정답 생성 및 시도값 저장변수 선언
 export let answer = generateNumbers(); 
 export let tries = []; 
+
+// 정답 입력 처리 함수
+function handleSubmit(event) {
+  event.preventDefault();
+  const value = $input.value;
+  const valid = checkInput(value);
+  
+  if (!valid) return; //잘못된 값 입력 시 
+
+  if (answer.join('')===value){ //정답 입력 시
+    displayLogText('🎉정답을 맞추셨습니다!🎉');
+    document.getElementById('restart').style.display='block';
+    $input.disabled=true;
+    $confirmButton.disabled=true;
+    return;
+  }
+  const score = calculateScore(value); //유효한 시도시
+  displayScore(value, score);
+  tries.push(value); 
+}
