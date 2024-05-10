70e10fe289b310de65a243783a01744e3008d17e
faddishcorn <juni129461@gmail.com>
2024-05-10 18:50:08 +0900

    chore: 정답 생성 및 시도값 저장변수 선언

--- src/main.js
+++ src/main.js
@@ -5,3 +5,7 @@ const $form = document.querySelector('#form');
 const $restartButton = document.querySelector('#restartButton');
 const $restart = document.querySelector('#restart');
 const $confirmButton = document.querySelector('#confirmButton');
+
+//정답 생성 및 시도값 저장변수 선언
+export let answer = generateNumbers(); 
+export let tries = []; 
