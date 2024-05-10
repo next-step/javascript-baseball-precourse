4e4745f8ba0d2594b850e07fa5ee186d5b57d2d8
rudtj <kyoungseo0327@gmail.com>
2024-05-10 14:44:38 +0900

    feat: 게임 로직 구조를 모듈화

--- src/main.js
+++ src/main.js
@@ -1,33 +1,7 @@
-import { gameResult } from "./gameResult";
-import { checkError } from "./checkError";
-import { randomNum } from "./\brandomNum";
+import inputNum from "./components/inputNum";
+import playGame from "./components/playGame";
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const userInput = document.querySelector('#userInput');
-    const confirmButton = document.querySelector('#confirmButton');
-    const restartButton = document.querySelector('#restartButton');
-
-    restartButton.addEventListener('click', () => window.location.reload());
-  
-    let answer = randomNum(); 
-    
-    confirmButton.addEventListener('click', () => {
-      // 입력값에 대한 에러 체크
-      const error = checkError(userInput.value);
-      if (error) {
-        alert(error); 
-        return;
-      }
-  
-      // 게임 결과 확인
-      const result = gameResult(userInput.value.split(''), answer);
-      alert(result.message); 
-  
-      // 정답을 맞췄을 경우, 게임 재시작 버튼을 화면에 추가
-      if (result.isCorrect) {
-        restartButton.style.display = 'block';
-      }
-    });
-  });
-
+function main() {
+  inputNum(playGame);
+}
+main();
