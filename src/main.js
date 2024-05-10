532982cb2e3468c9460e128b7bc63fe1f9443988
faddishcorn <juni129461@gmail.com>
2024-05-10 18:48:42 +0900

    chore: 변수 선언

---
+++ src/main.js
@@ -0,0 +1,7 @@
+//DOM 요소
+export const $input = document.querySelector('#input');
+export const $logs = document.querySelector('#logs');
+const $form = document.querySelector('#form');
+const $restartButton = document.querySelector('#restartButton');
+const $restart = document.querySelector('#restart');
+const $confirmButton = document.querySelector('#confirmButton');
